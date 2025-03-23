fb246f3ccd1794f0676b0157fce8172b2133f6aa
Daniel Sousa <daniel_sousa.p@outlook.com>
2025-03-23 16:51:05 -0300

    feat: create domain layer

---
+++ internal/domain/short_url.go
@@ -0,0 +1,10 @@
+package domain
+
+type UrlMapping struct {
+	LongURL  string `json:"long_url" validate:"required"`
+	ShortURL string `json:"short_url" validate:"required"`
+}
+
+type ShortUrlRequest struct {
+	URL string `json:"url" validate:"required"`
+}
