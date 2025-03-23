5a12939472b178ad3c35849e8f53678341dae26d
Daniel Sousa <daniel_sousa.p@outlook.com>
2025-03-23 16:51:54 -0300

    feat: create router setup

---
+++ internal/router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"github.com/danniels/shortening-url/internal/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(h *handler.Handler) *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/shorten", h.ShortUrl)
+	r.GET("/:shorted_url", h.RedirectUrl)
+
+	return r
+}
